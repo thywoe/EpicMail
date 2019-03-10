fcaa1d121b48e9056d5f81a1e8482438c66d158c
Babalola Taiwo <bamibabson@gmail.com>
2019-03-10 17:13:38 +0100

    Add data structure to store data

---
+++ Model/db.js
@@ -0,0 +1,15 @@
+const mails = {
+    messages : [{
+        id: 1,
+        createdOn: 2019,
+        subject: "Learning",
+        message: "Learn to re-learn",
+        senderId: 1,
+        receiverId: 2,
+        parentMessageId: 2,
+        status: "sent"
+    }
+]
+}
+
+export default mails;
